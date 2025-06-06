c8a095b5364b70c7bba81a354a5c5c65fdaa919d
Verseler <vrslr2003@gmail.com>
2025-06-06 15:50:00 +0800

    feat: broadcasting

---
+++ echo.js
@@ -0,0 +1,33 @@
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+const echo = new Echo({
+  broadcaster: "pusher",
+  key: "localkey123", // same as REVERB_APP_KEY
+  wsHost: "127.0.0.1",
+  wsPort: 443,
+  forceTLS: false,
+  disableStats: true,
+  enabledTransports: ["ws", "wss"],
+});
+
+export default echo;
+
+
+// import Echo from 'laravel-echo';
+
+// import Pusher from 'pusher-js';
+// window.Pusher = Pusher;
+
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'],
+// });
+
